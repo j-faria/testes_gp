fd88dec5da22a65786c3f5d9a494e6a13d61edc7
João Camacho <jdavidrcamacho@gmail.com>
2016-09-05 10:29:15 +0100

    Update 03_Exemplo_CicloSolar.py

--- 03_Exemplo_CicloSolar.py
+++ 03_Exemplo_CicloSolar.py
@@ -12,11 +12,13 @@ import matplotlib.pyplot as pl
 pl.close("all") #fecha todas as figuras anteriores
 
 data= np.loadtxt('SN_m_tot_V2.0.txt') #data[linha,coluna]
-t=data[2965:3120,2] #ciclo 23
-y=data[2965:3120,3]
+t=data[:,2] #todos os ciclos
+y=data[:,3]
+#t=data[2965:3120,2] #ciclo 23
+#y=data[2965:3120,3]
 #t=data[2965:-1,2] #ciclo 23 + ciclo 24
 #y=data[2965:-1,3]
-yerr = 0.2*np.ones_like(t)
+yerr = np.ones_like(t)
 
 pl.figure()
 pl.plot(t,y,'.')
@@ -28,9 +30,10 @@ from george import kernels, GP, HODLRSolver
 import george
 
 #k1 = ExpSquaredKernel(200.0**2)
-k2 = (100**2)*kernels.ExpSine2Kernel((2.0/0.01)**2,11)
+k2 = 100**2*kernels.ExpSine2Kernel((2.0/0.5)**2,135)
+K3 = 100**2*kernels.ExpSine2Kernel((2.0/1)**2,1200)
 
-k3 = kernels.WhiteKernel(0.2)    
+#k0 = kernels.WhiteKernel(50)    
 
 kernel = k2 + k3
 
@@ -48,7 +51,8 @@ x=t
 #x = np.linspace(1996.124, 2008.958, 155) #ciclo 23
 #x = np.linspace(min(t), max(t), len(t))
 #x = np.linspace(1996.5, 2009, len(t))
-#x = np.linspace(max(t),2025,(2025-max(t))*12) #previsao ?
+#x = np.linspace(max(t),2050,(2050-max(t))*12) #previsao ?
+#x=np.linspace(min(t),2050,(2050-min(t))*12)
 mu, cov = gp.predict(y, x) #mean mu and covariance cov
 std = np.sqrt(np.diag(cov))
 
@@ -57,9 +61,13 @@ pl.fill_between(x, mu+std, mu-std, color="k", alpha=0.1)
 pl.plot(x, mu+std, color="k", alpha=1, lw=0.25)
 pl.plot(x, mu-std, color="k", alpha=1, lw=0.25)
 pl.plot(x, mu, color="r", alpha=1, lw=0.9)
-pl.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+pl.errorbar(x, y, yerr=yerr,xerr=None, fmt=".k", capsize=0)
 pl.xlabel("$x$")
 pl.ylabel("$y$")
+
+
+
+
 
 
 
