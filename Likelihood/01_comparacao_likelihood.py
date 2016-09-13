b03164cb27f6d33ad6b433cc382e3735e0f4f077
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-09-13 13:27:22 +0100

    comparacao entre likelihood

--- Likelihood/01_comparacao_likelihood.py
+++ Likelihood/01_comparacao_likelihood.py
@@ -72,6 +72,6 @@ ystar_var = np.dot(np.dot(K_star,K_inv),K_star.T)
 
 #Calculo da log likelihood
 n=len(x)
-log_p2 = -0.5*np.dot(np.dot(np.dot(y.T,L.T),L_inv),y) - sum(np.log(np.diag(L))) \
+log_p = -0.5*np.dot(np.dot(np.dot(y.T,L.T),L_inv),y) - sum(np.log(np.diag(L))) \
             - n*0.5*np.log(2*np.pi)            
-print(log_p2)
+print(log_p)
