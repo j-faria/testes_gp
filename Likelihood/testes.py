98120c1637bc3226a263a1edbeb65847f3cddbc9
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-09-14 11:47:34 +0100

    rascunho

---
+++ Likelihood/testes.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 14:45:00 2016
+
+@author: camacho
+"""
+##### RASCUNHO - ignorar o que for feito aqui #####
+
+import numpy as np
+from Kernel import *
+
+
+x = [-1.5, -1, -0.75, -0.4, -0.25, 0]
+y = [0.55*-3, 0.55*-2, 0.55*-0.6, 0.55*0.4, 0.55*1, 0.55*1.6]
+yerr=0.3 * np.ones_like(x)
+
+#pl.plot(x,y,"*")
+
+########## definir kernel a usar e parametros #########
+k1 = ExpSquared(1,1,4,3) 
+#k2 = ExpSquared(2,5,x1=x,x2=x)
+#kernel=  k1+k2
+
+
+#K=np.zeros((len(x),len(x)))
+#for i in range(len(x)):
+#    for j in range(len(x)):
+#        K[i,j]=kernel('theta','l',x1[i],x2[j])
+#K=K+yerr**2*np.identity(len(x))      
+
+def f(x,y):
+    return x**2 + y
+def g(z):
+    return z**3
+    
+def h(x,y,z):
+    return f(x,y)+g(z)
