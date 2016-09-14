59c25ddeaca01459f720767d0e01f96c5ecebb37
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-09-14 15:04:59 +0100

    Kernel desenvolvida

--- Likelihood/Kernel.py
+++ Likelihood/Kernel.py
@@ -10,15 +10,8 @@ import numpy as np
 def ExpSquared(x1, x2, ES_theta, ES_l):
     f1 = ES_theta**2
     f2 = ES_l**2
-    f3 = (x1-x2)**2
+    f3 = (np.array(x1)-np.array(x2))**2
     return f1*np.exp(-0.5*f3/f2)
-#def  ExpSquared(*args):
-#    theta,l,x1,x2 = args
-#    f1 = theta**2
-#    f2 = l**2
-#    f3 = (x1-x2)**2
-#    return f1*np.exp(-0.5*f3/f2)   
- 
    
 # Rational Quadratic Kernel
 def RatQuadratic(x1, x2, RQ_theta, RQ_l, RQ_alpha):
@@ -42,12 +35,19 @@ def Local_ExpSineSquared(x1, x2, LESS_theta, LESS_l, LESS_P):
     f4 = (x1-x2)**2
     return f1*np.exp(-2*(np.sin(np.pi*f3/LESS_P))**2/f2)*np.exp(-0.5*f4/f2)
 
-# Linear Kernel
-def Linear(x1, x2,L_thetab,L_thetav,L_c):
-    f1 = L_thetab**2
-    f2 = L_thetav**2
-    return f1+f2*(x1-L_c)*(x2-L_c)
+## Linear Kernel
+#def Linear(x1, x2,L_thetab,L_thetav,L_c):
+#    f1 = L_thetab**2
+#    f2 = L_thetav**2
+#    return f1+f2*(x1-L_c)*(x2-L_c)
     
 #Soma de Periodic com Squared Exponential (ExpSineSquared + ExpSquared)
 def Sum_ExpSineSquared_ExpSquared(x1, x2, ESS_theta, ESS_l, ESS_P, ES_theta, ES_l):
     return ExpSineSquared(x1,x2,ESS_theta,ESS_l,ESS_P) + ExpSquared(x1,x2,ES_theta,ES_l)
+    
+# Squared Exponential Kernel com white noise
+def ExpSquared_WN(x1, x2, ES_theta, ES_l, WN):
+    f1 = ES_theta**2
+    f2 = ES_l**2
+    f3 = (x1-x2)**2
+    return f1*np.exp(-0.5*f3/f2)
