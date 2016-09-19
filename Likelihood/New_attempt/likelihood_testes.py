7b40683d7cfbd1e8634a47e8c1546feb10640cbf
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-09-19 15:31:40 +0100

    likelihood tests to see if it works

--- Likelihood/New_attempt/likelihood_testes.py
+++ Likelihood/New_attempt/likelihood_testes.py
@@ -11,26 +11,30 @@ from likelihood import *
 x = 10 * np.sort(np.random.rand(2000))
 yerr = 0.2 * np.ones_like(x)
 y = np.sin(x) + yerr * np.random.randn(len(x))
-###############################################################################
 
 x1=x
 x2=x
-#considerando a kernel ExpSquared com parametros:
-theta1 = 1
-l1 = 1
-#cosiderando a kernel ExpSineSQuared com parametros:
-theta2= 10
-l2=1
-P2=5
+###############################################################################
+#likelihood(kernels, x dado, x a calcular, y, yerr)
+
 #   Os parametros foram dados às três pancadas só mesmo para ver se o python faz
 #os calculos sem dar erro, por isso a likelihood deverá dar valores estranhos.
 
+#kernel sozinha
+print('-> lonely kernel')
+likelihood(ExpSquared(19,2), x, x, y, yerr)
 
-#likelihood(kernels, x dado, x a calcular, y, yerr)
-likelihood(ExpSquared(theta1,l1), x, x, y, yerr)
-
+print('-> sum of kernels')
 #somar
-likelihood(ExpSquared(theta1,l1)+ExpSineSquared(theta2,l2,P2), x, x, y, yerr)
+likelihood(ExpSquared(10,1)+ExpSineSquared(1,1,5), x, x, y, yerr)
 
+print('-> multiplication of kernels')
 #multiplicar
-likelihood(ExpSquared(theta1,l1)*ExpSineSquared(theta2,l2,P2), x, x, y, yerr)
+likelihood(ExpSquared(10,1)*ExpSineSquared(1,1,5), x, x, y, yerr)
+likelihood(Local_ExpSineSquared(10,1,5), x,  x, y ,yerr)
+#   A Local_ExpSineSquared = ExpSquared * ExpSineSquared, logo se a
+#multiplicação estiver a ser bem feita as duas devem dar os mesmos valores
+
+print('-> multiplication and sum of kernels')
+#multiplicar com white noise incluido
+likelihood(ExpSquared(theta1,l1)*ExpSineSquared(theta2,l2,P2) +WhiteNoise(2), x, x, y, yerr)
