7dc96342771f96a77b3fb49144f0ea75c39c5918
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-09-20 17:03:19 +0100

    tests

--- Likelihood/New_attempt/likelihood_testes.py
+++ Likelihood/New_attempt/likelihood_testes.py
@@ -5,15 +5,14 @@ Created on Mon Sep 19 10:56:24 2016
 @author: camacho
 """
 
-from likelihood import *
+import Kernel as kl
+import likelihood as lh
 
 #####  DADOS INICIAS  #########################################################
-x = 10 * np.sort(np.random.rand(2000))
+x = 10 * np.sort(np.random.rand(20))
 yerr = 0.2 * np.ones_like(x)
 y = np.sin(x) + yerr * np.random.randn(len(x))
 
-x1=x
-x2=x
 ###############################################################################
 #likelihood(kernels, x dado, x a calcular, y, yerr)
 
@@ -21,20 +20,21 @@ x2=x
 #os calculos sem dar erro, por isso a likelihood deverá dar valores estranhos.
 
 #kernel sozinha
-print('-> lonely kernel')
-likelihood(ExpSquared(19,2), x, x, y, yerr)
+#print('-> lonely kernel')
+#lh.likelihood(x, x, y, yerr, par2,par1)
+lh.likelihood(kl.ExpSquared(19,2),x,x,y,yerr)
 
-print('-> sum of kernels')
-#somar
-likelihood(ExpSquared(10,1)+ExpSineSquared(1,1,5), x, x, y, yerr)
-
-print('-> multiplication of kernels')
-#multiplicar
-likelihood(ExpSquared(10,1)*ExpSineSquared(1,1,5), x, x, y, yerr)
-likelihood(Local_ExpSineSquared(10,1,5), x,  x, y ,yerr)
-#   A Local_ExpSineSquared = ExpSquared * ExpSineSquared, logo se a
-#multiplicação estiver a ser bem feita as duas devem dar os mesmos valores
-
-print('-> multiplication and sum of kernels')
-#multiplicar com white noise incluido
-likelihood(ExpSquared(10,1)*ExpSineSquared(1,1,5) +WhiteNoise(2), x, x, y, yerr)
+#print('-> sum of kernels')
+##somar
+#likelihood(ExpSquared(10,1)+ExpSineSquared(1,1,5), x, x, y, yerr)
+#
+#print('-> multiplication of kernels')
+##multiplicar
+#likelihood(ExpSquared(10,1)*ExpSineSquared(1,1,5), x, x, y, yerr)
+#likelihood(Local_ExpSineSquared(10,1,5), x,  x, y ,yerr)
+##   A Local_ExpSineSquared = ExpSquared * ExpSineSquared, logo se a
+##multiplicação estiver a ser bem feita as duas devem dar os mesmos valores
+#
+#print('-> multiplication and sum of kernels')
+##multiplicar com white noise incluido
+#likelihood(ExpSquared(10,1)*ExpSineSquared(1,1,5) +WhiteNoise(2), x, x, y, yerr)
