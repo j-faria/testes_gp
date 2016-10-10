cbf68d4d37ab44842e98726ddc7e3dfbfc2f443a
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-10-10 12:21:31 +0100

    tests to see if python works

--- Likelihood/New_attempt/likelihood_testes_2.py
+++ Likelihood/New_attempt/likelihood_testes_2.py
@@ -17,9 +17,9 @@ yerr1 = 0.2 * np.ones_like(x1)
 y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
 
 
-kernel0=kl.ExpSquared(19.0, 2.0)
-kl.likelihood(kernel0, x1, x1, y1, yerr1)
-kl.gradient_likelihood(kernel0, x1, x1, y1, yerr1)
+kernel1=kl.ExpSquared(19.0, 2.0)
+kl.likelihood(kernel1, x1, x1, y1, yerr1)
+kl.gradient_likelihood(kernel1, x1, x1, y1, yerr1)
 
 # Calculation using george 
 start = time() 
@@ -47,3 +47,21 @@ gp = george.GP(kernelg2)
 gp.compute(x2,yerr2)
 print 'Took %f seconds' % (time() - start), ('log_p_george',gp.lnlikelihood(y2))
 print 'gradient_george ->', gp.grad_lnlikelihood(y2)
+
+
+### EXEMPLO 3 - ExpSquared + ExpSineSquared
+x3 = 10 * np.sort(np.random.rand(103))
+yerr3 = 0.2 * np.ones_like(x3)
+y3 = np.sin(x3) + yerr3 * np.random.randn(len(x3))
+
+kernel3=kl.ExpSquared(19.0, 2.0) + kl.ExpSineSquared(15.0, 1.0, 10.0)
+kl.likelihood(kernel3, x3, x3, y3, yerr3)
+kl.gradient_likelihood(kernel3, x3, x3, y3, yerr3)
+
+# Calculation using george
+start = time()  
+kernelg3 = 19.0**2*ExpSquaredKernel(2.0**2) + 15.0**2*ExpSine2Kernel(2.0/1.0**2,10.0)
+gp = george.GP(kernelg3)
+gp.compute(x3,yerr3)
+print 'Took %f seconds' % (time() - start), ('log_p_george',gp.lnlikelihood(y3))
+print 'gradient_george ->', gp.grad_lnlikelihood(y3)
