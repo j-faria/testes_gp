e7d3cf6fb58301c0644aae1d77e77059f5225c42
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-10-25 15:26:15 +0100

    where the math is made

--- Programs made/Likelihood.py
+++ Programs made/Likelihood.py
@@ -70,13 +70,13 @@ def gradient_likelihood(kernel,x,xcalc,y,yerr):
         grad1=grad_logp(kernel.dES_dtheta, x, xcalc, y, yerr, cov_matrix)
         grad2=grad_logp(kernel.dES_dl, x, xcalc, y, yerr, cov_matrix)
         print 'gradient ->', grad1, grad2
-        #return grad1, grad2
+        return grad1, grad2
     elif isinstance(kernel,kl.ExpSineSquared):
         grad1=grad_logp(kernel.dESS_dtheta,x,xcalc,y,yerr,cov_matrix)
         grad2=grad_logp(kernel.dESS_dl,x,xcalc,y,yerr,cov_matrix)
         grad3=grad_logp(kernel.dESS_dP,x,xcalc,y,yerr,cov_matrix)
-        print 'gradient ->', grad1, grad2, grad3
-        #return grad1, grad2, grad3 
+        #print 'gradient ->', grad1, grad2, grad3
+        return grad1, grad2, grad3 
     elif isinstance(kernel,kl.RatQuadratic):
         grad1=grad_logp(kernel.dRQ_dtheta,x,xcalc,y,yerr,cov_matrix)
         grad2=grad_logp(kernel.dRQ_dalpha,x,xcalc,y,yerr,cov_matrix)
@@ -94,33 +94,45 @@ def gradient_likelihood(kernel,x,xcalc,y,yerr):
         print 'gradient ->', grad1, grad2
     elif isinstance(kernel,kl.Sum):
         gradient_sum(kernel,x,xcalc,y,yerr)
-        
+    elif isinstance(kernel,kl.Product):
+        gradient_mul(kernel,x,xcalc,y,yerr)
+                
     else:
         print 'gradient -> We dont need no calculation  \n            We dont need no optimization control'    
     #   Nao apliquei a mesma logica às kernels exponential e matern pois
     #até isto funcionar como deve ser não vale a pena fazer
     #funcionar como deve ser = saber se estou a calcular o gradiente bem
     #e arranjar maneira de isto funcionar com somas e multiplicaçoes de kernels
-      
+ 
+
+#EM TESTES - IGNORAR POR ENQUANTO     
 def gradient_sum(kernel,x,xcalc,y,yerr):
-    print "It is a work in progress"
-#    from numpy import arange
-#    #a0=kernel.parSize()
-#    a=kernel.__dict__
-#    grad_result=[]    
-#    for i in arange(1,len(kernel.__dict__)+1):
-#        #inv_map = {v: k for k, v in a.iteritems()}
-#        #print a, inv_map
-#        #k_i = a.popitem();
-#        var = "k%i" %i
-#        k_i = a[var]
-#        print var, k_i
-#        calc = gradient_likelihood(k_i,x,xcalc,y,yerr)
-#        #print  calc
-#        grad_result.insert(0,calc)
-#        grad_final  =[]
-#        for j in range(len(grad_result)):         
-#           grad_final = grad_final + list(grad_result[j])
-#    print 'gradient sum ->', grad_final
-#    return grad_final
-##Devolve NoneType, rever!
+    print 'Work in progress'
+    from numpy import arange
+    a=kernel.__dict__
+    grad_result=[]    
+    for i in arange(1,len(kernel.__dict__)+1):
+        var = "k%i" %i
+        k_i = a[var]
+        #print var, k_i
+        calc = gradient_likelihood(k_i,x,xcalc,y,yerr)
+        #print  'calculo', calc
+        grad_result.insert(1,calc)
+        grad_final  =[]
+        for j in range(len(grad_result)):         
+           grad_final = grad_final + list(grad_result[j])
+    print 'gradient sum ->', grad_final
+    return grad_final
+#Devolve NoneType -> acontece se faltar return no gradient_likelihood
+    
+def gradient_mul(kernel,x,xcalc,y,yerr):
+    print 'Work in progress'
+    from numpy import arange
+    a=kernel.__dict__
+    grad_result=[]
+    for i in arange(1,len(kernel.__dict__)+1):
+        var = "k%i" %i
+        k_i = a[var]
+        print k_i        
+        var = "k%i" %i
+        k_i = a[var]     
--- Programs made/tests.py
+++ Programs made/tests.py
@@ -14,20 +14,20 @@ import george
 import george.kernels as ge
 
 
-#### EXEMPLO 1 - ExpSquared
+### EXEMPLO 1 - ExpSquared
 #np.random.seed(1001)
 #x1 = 10 * np.sort(np.random.rand(101))
 #yerr1 = 0.2 * np.ones_like(x1)
 #y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
 #
 ## Calculation using our stuff
-#print '########## ExpSquared'
-#kernel1=kl.ExpSquared(19.0, 1.3)
+#print '########## EXPSQUARED ##########'
+#kernel1=kl.ExpSquared(19.0, 2.0)
 #lk.likelihood(kernel1, x1, x1, y1, yerr1)
 #lk.gradient_likelihood(kernel1, x1, x1, y1, yerr1)
-#
+
 ## Calculation using george 
-#kernelg1 = 19.0**2*ge.ExpSquaredKernel(1.3**2)
+#kernelg1 = 19.0**2*ge.ExpSquaredKernel(2.0**2)
 #gp = george.GP(kernelg1)
 #gp.compute(x1,yerr1)
 #print 'likelihood_george ->', gp.lnlikelihood(y1)
@@ -41,22 +41,22 @@ import george.kernels as ge
 ###############################################################################
 
 ### EXEMPLO 2 - ExpSineSquared
-np.random.seed(1001)
-x2 = 10 * np.sort(np.random.rand(102))
-yerr2 = 0.2 * np.ones_like(x2)
-y2 = np.sin(x2) + yerr2 * np.random.randn(len(x2))
-
-print  '########## exemplo igual ao george'
-kernel=kl.ExpSineGeorge(2.0/1.1**2,7.0)
-lk.likelihood(kernel, x2, x2, y2, yerr2)
-lk.gradient_likelihood(kernel, x2, x2, y2, yerr2)
-# Calculation using george
-kernelgeo = ge.ExpSine2Kernel(2.0/1.1**2,7.0)
-gp = george.GP(kernelgeo)
-gp.compute(x2,yerr2)
-print 'likelihood_george ->', gp.lnlikelihood(y2)
-print 'gradient_george ->', gp.grad_lnlikelihood(y2)
-#print 'acho que devia dar tudo igual, não dá'
+#np.random.seed(1001)
+#x2 = 10 * np.sort(np.random.rand(102))
+#yerr2 = 0.2 * np.ones_like(x2)
+#y2 = np.sin(x2) + yerr2 * np.random.randn(len(x2))
+#
+#print  '########## EXEMPLO IGUAL AO GEORGE ##########'
+#kernel=kl.ExpSineGeorge(2.0/1.1**2,7.0)
+#lk.likelihood(kernel, x2, x2, y2, yerr2)
+#lk.gradient_likelihood(kernel, x2, x2, y2, yerr2)
+## Calculation using george
+#kernelgeo = ge.ExpSine2Kernel(2.0/1.1**2,7.0)
+#gp = george.GP(kernelgeo)
+#gp.compute(x2,yerr2)
+#print 'likelihood_george ->', gp.lnlikelihood(y2)
+#print 'gradient_george ->', gp.grad_lnlikelihood(y2)
+##print 'acho que devia dar tudo igual, não dá'
 
 
 #xx2,_ = gp.parse_samples(x2)
@@ -66,7 +66,7 @@ print 'gradient_george ->', gp.grad_lnlikelihood(y2)
 
 
 ## Calculation using our stuff
-#print '########## ExpSineSquared'
+#print '########## EXPSINESQUARED'
 #kernel2=kl.ExpSineSquared(15.0, 2.0, 10.0)
 #lk.likelihood(kernel2, x2, x2, y2, yerr2)
 #lk.gradient_likelihood(kernel2, x2, x2, y2, yerr2)
@@ -85,6 +85,7 @@ print 'gradient_george ->', gp.grad_lnlikelihood(y2)
 #yerr3 = 0.2 * np.ones_like(x3)
 #y3 = np.sin(x3) + yerr3 * np.random.randn(len(x3))
 #
+#print '########## RATQUADRATIC ##########'
 #kernel3=kl.RatQuadratic(1.0,1.5,1.0)
 #lk.likelihood(kernel3, x3, x3, y3, yerr3)
 #lk.gradient_likelihood(kernel3, x3, x3, y3, yerr3)
@@ -95,22 +96,21 @@ print 'gradient_george ->', gp.grad_lnlikelihood(y2)
 #gp.compute(x3,yerr3)
 #print 'likelihood_george ->', gp.lnlikelihood(y3)
 #print 'gradient_george ->', gp.grad_lnlikelihood(y3)
-
 ###############################################################################
 
-#### EXEMPLO 10 - ExpSquared + ExpSineSquared
-#x1 = 10 * np.sort(np.random.rand(103))
-#yerr1 = 0.2 * np.ones_like(x1)
-#y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
-#
-#print '### soma'
-#kernel3=kl.ExpSquared(19.0, 2.0) + kl.ExpSineSquared(15.0, 1.0, 10.0)
-#lk.likelihood(kernel3, x1, x1, y1, yerr1)
-#lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
-#
-## Calculation using george
-#kernelg3 = 19.0**2*ge.ExpSquaredKernel(2.0**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.0**2,10.0)
-#gp = george.GP(kernelg3)
-#gp.compute(x1,yerr1)
-#print 'likelihood_george ->',  gp.lnlikelihood(y1)
-#print 'gradient_george ->', gp.grad_lnlikelihood(y1)
+### EXEMPLO 10 - ExpSquared + ExpSineSquared
+x1 = 10 * np.sort(np.random.rand(103))
+yerr1 = 0.2 * np.ones_like(x1)
+y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
+
+print '########## SOMA ##########'
+kernel3=kl.ExpSquared(19.0, 2.0) * kl.ExpSineSquared(15.0, 1.0, 10.0) 
+lk.likelihood(kernel3, x1, x1, y1, yerr1)
+lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
+
+# Calculation using george
+kernelg3 = 19.0**2*ge.ExpSquaredKernel(2.0**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.0**2,10.0)
+gp = george.GP(kernelg3)
+gp.compute(x1,yerr1)
+print 'likelihood_george ->',  gp.lnlikelihood(y1)
+print 'gradient_george ->', gp.grad_lnlikelihood(y1)
