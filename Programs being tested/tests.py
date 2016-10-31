544eb44d4b7c86e29258ae1af6621912acaff69a
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-10-31 11:19:26 +0000

    trials

--- Programs being tested/tests.py
+++ Programs being tested/tests.py
@@ -14,27 +14,27 @@ import george
 import george.kernels as ge
 
 
-#EXAMPLE OURS-GEORGE USING ExpSineSquared
-np.random.seed(1001)
-x2 = 10 * np.sort(np.random.rand(102))
-yerr2 = 0.2 * np.ones_like(x2)
-y2 = np.sin(x2) + yerr2 * np.random.randn(len(x2))
+###EXAMPLE OURS-GEORGE USING ExpSineSquared
+#np.random.seed(1001)
+#x2 = 10 * np.sort(np.random.rand(102))
+#yerr2 = 0.2 * np.ones_like(x2)
+#y2 = np.sin(x2) + yerr2 * np.random.randn(len(x2))
+##pl.plot(x2,y2)
+#
+#print  '########## EXEMPLE EQUAL TO GEORGE ##########'
+#kernel=kl.ExpSineGeorge(2.0/1.1**2, 7.1)
+#lk.likelihood(kernel, x2, x2, y2, yerr2)
+#lk.gradient_likelihood(kernel, x2, x2, y2, yerr2)
+#
+## Calculation using george
+#kernelgeo = ge.ExpSine2Kernel(2.0/1.1**2, 7.1)
+#gp = george.GP(kernelgeo)
+#gp.compute(x2,yerr2)
+#print 'likelihood_george ->', gp.lnlikelihood(y2)
+#print 'gradient_george ->', gp.grad_lnlikelihood(y2)
+###############################################################################
 
-print  '########## EXEMPLE EQUAL TO GEORGE ##########'
-kernel=kl.ExpSineGeorge(2.0/1.1**2, 7.1)
-lk.likelihood(kernel, x2, x2, y2, yerr2)
-lk.gradient_likelihood(kernel, x2, x2, y2, yerr2)
-
-# Calculation using george
-kernelgeo = ge.ExpSine2Kernel(2.0/1.1**2, 7.1)
-gp = george.GP(kernelgeo)
-gp.compute(x2,yerr2)
-print 'likelihood_george ->', gp.lnlikelihood(y2)
-print 'gradient_george ->', gp.grad_lnlikelihood(y2)
-
-
-##### IGNORE FROM HERE ########################################################
-### EXEMPLO 1 - ExpSquared
+## EXEMPLO 1 - ExpSquared
 #np.random.seed(1001)
 #x1 = 10 * np.sort(np.random.rand(101))
 #yerr1 = 0.2 * np.ones_like(x1)
@@ -45,27 +45,16 @@ print 'gradient_george ->', gp.grad_lnlikelihood(y2)
 #kernel1=kl.ExpSquared(19.0, 2.0)
 #lk.likelihood(kernel1, x1, x1, y1, yerr1)
 #lk.gradient_likelihood(kernel1, x1, x1, y1, yerr1)
-
+#
 ## Calculation using george 
 #kernelg1 = 19.0**2*ge.ExpSquaredKernel(2.0**2)
 #gp = george.GP(kernelg1)
 #gp.compute(x1,yerr1)
 #print 'likelihood_george ->', gp.lnlikelihood(y1)
 #print 'gradient_george ->', gp.grad_lnlikelihood(y1)
-
-
-##Devolve a matrix do gradiente no george
-#xx1,_ = gp.parse_samples(x1)
-#print gp.kernel.gradient(xx1).shape
-#print gp.kernel.gradient(xx1)[:,:,0]
 ###############################################################################
 
-#xx2,_ = gp.parse_samples(x2)
-#print gp.kernel.gradient(xx2).shape
-#print gp.kernel.gradient(xx2)[:,:,0]
-#print gp.kernel.gradient(xx2)[:,:,1]
-
-
+## EXEMPLO 2 - ExpSineSquared
 ## Calculation using our stuff
 #print '########## EXPSINESQUARED'
 #kernel2=kl.ExpSineSquared(15.0, 2.0, 10.0)
@@ -99,37 +88,48 @@ print 'gradient_george ->', gp.grad_lnlikelihood(y2)
 #print 'gradient_george ->', gp.grad_lnlikelihood(y3)
 ###############################################################################
 
-#### EXEMPLO 10 - ExpSquared + ExpSineSquared
-#x1 = 10 * np.sort(np.random.rand(103))
-#yerr1 = 0.2 * np.ones_like(x1)
-#y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
-#
-#print '########## SOMA ##########'
-#kernel3=kl.ExpSquared(19.0, 2.0) + kl.ExpSineSquared(15.0, 1.0, 10.0) 
-#lk.likelihood(kernel3, x1, x1, y1, yerr1)
-#lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
-#
-## Calculation using george
-#kernelg3 = 19.0**2*ge.ExpSquaredKernel(2.0**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.0**2,10.0)
-#gp = george.GP(kernelg3)
-#gp.compute(x1,yerr1)
-#print 'likelihood_george ->',  gp.lnlikelihood(y1)
-#print 'gradient_george ->', gp.grad_lnlikelihood(y1)
+### EXEMPLO 10 - ExpSquared + ExpSineSquared
+x1 = 10 * np.sort(np.random.rand(103))
+yerr1 = 0.2 * np.ones_like(x1)
+y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
+
+print '########## SOMA ES+ESS ##########'
+kernel3=kl.ExpSquared(19.1, 1.3) + kl.ExpSineSquared(15.0, 1.5, 11.0) 
+lk.likelihood(kernel3, x1, x1, y1, yerr1)
+lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
+
+# Calculation using george
+kernelg3 = 19.1**2*ge.ExpSquaredKernel(1.3**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.5**2,11.0)
+gp = george.GP(kernelg3)
+gp.compute(x1,yerr1)
+print 'likelihood_george ->',  gp.lnlikelihood(y1)
+print 'gradient_george ->', gp.grad_lnlikelihood(y1)
 ###############################################################################
 
-#### EXEMPLO 11 - ExpSquared * ExpSineSquared
-#x1 = 10 * np.sort(np.random.rand(103))
-#yerr1 = 0.2 * np.ones_like(x1)
-#y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
-#
-#print '########## MULTIPLICACAO ##########'
-#kernel3=kl.ExpSquared(19.0, 2.0) * kl.ExpSineSquared(15.0, 1.0, 10.0) 
-#lk.likelihood(kernel3, x1, x1, y1, yerr1)
-#lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
+### EXEMPLO 11 - RatQuadratic + ExpSineSquared
+x1 = 10 * np.sort(np.random.rand(103))
+yerr1 = 0.2 * np.ones_like(x1)
+y1 = np.sin(x1) + yerr1 * np.random.randn(len(x1))
 
-## Calculation using george
-#kernelg3 = 19.0**2*ge.ExpSquaredKernel(2.0**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.0**2,10.0)
-#gp = george.GP(kernelg3)
-#gp.compute(x1,yerr1)
-#print 'likelihood_george ->',  gp.lnlikelihood(y1)
-#print 'gradient_george ->', gp.grad_lnlikelihood(y1)
+print '########## Soma RQ+ESS ##########'
+kernel3=kl.RatQuadratic(11.0,1.5,1.0) + kl.ExpSineSquared(15.0, 1.0, 10.0) 
+lk.likelihood(kernel3, x1, x1, y1, yerr1)
+lk.gradient_likelihood(kernel3, x1, x1, y1, yerr1)
+
+# Calculation using george
+kernelg3 = 11.0**2*ge.RationalQuadraticKernel(1.5,1.0**2) + 15.0**2*ge.ExpSine2Kernel(2.0/1.0**2,10.0)
+gp = george.GP(kernelg3)
+gp.compute(x1,yerr1)
+print 'likelihood_george ->',  gp.lnlikelihood(y1)
+print 'gradient_george ->', gp.grad_lnlikelihood(y1)
+###############################################################################
+
+##Devolve a matrix do gradiente no george
+#xx1,_ = gp.parse_samples(x1)
+#print gp.kernel.gradient(xx1).shape
+#print gp.kernel.gradient(xx1)[:,:,0]
+#
+#xx2,_ = gp.parse_samples(x2)
+#print gp.kernel.gradient(xx2).shape
+#print gp.kernel.gradient(xx2)[:,:,0]
+#print gp.kernel.gradient(xx2)[:,:,1]
