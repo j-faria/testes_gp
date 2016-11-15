4eefb79d4fcb1cd7f11bd971e8d809451c13a3fd
jdavidrcamacho <jdavidrcamacho@gmail.com>
2016-11-15 11:48:44 +0000

    more tests - ignore it for now :3

---
+++ Programs being tested/Gausspack.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 09:53:09 2016
+
+@author: camacho
+"""
+
+#PACOTINHO PARA IMPORTAR TUDO
+
+#importar todas as kernels
+from Kernel import *
+
+#importar calculos da likelihood, gradient e optimization
+from Likelihood import *
